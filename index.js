2a748bc4ccf085a59b6740845d112dc7bdcbb24c
Shaunish123 <shaunishs@gmail.com>
2024-07-31 17:16:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var img1 = document.querySelector("#img1");
+img1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+
+var img2 = document.querySelector("#img2");
+img2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+var refresh = document.querySelector("h1");
+if(randomNumber1>randomNumber2){
+    refresh.innerHTML="🏁 Player 1 wins";
+}
+
+else if(randomNumber2>randomNumber1){
+    refresh.innerHTML="Player 2 wins 🏁";
+}
+else{
+    refresh.innerHTML="Draw, try again!";
+}
